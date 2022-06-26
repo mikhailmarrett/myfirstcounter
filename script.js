df0041c3d8c9d7b725b2847c3ffac46d140ea938
Mikhail Marrett <mikhailmarrett@Mikhails-MacBook-Air.local>
2022-06-26 11:43:03 -0400

    update script

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
 const counterEl = document.getElementById("counter")
 const totalCount = document.getElementById("total")
 let count = 0
-let countHi = 0
-let countLow = 0
 let countSum = 0
-
 
 function textColor() {
   if (count > 0) {
@@ -19,31 +16,41 @@ function textColor() {
 function increase() {
   count += 1
   countHi = count
-  counterEl.innerHTML = count
+  counterEl.innerHTML = countHi
   textColor()
 }
 
 function decrease() {
   count -= 1
   countLow = count
+  counterEl.innerHTML = countLow
+  textColor()
+}
+
+function clearOut() {
+  count = 0
+  countHi = 0
+  countLow = 0
+  countSum = 0
   counterEl.innerHTML = count
+  totalCount.innerHTML = ""
   textColor()
 }
 
 function reset() {
-  count = countSum
   count = 0
   counterEl.innerHTML = count
   totalCount.innerHTML = ""
   textColor()
 }
 
+function getSum() {
+  countSum = countHi + countLow
+}
+
 function printTotal() {
-x = countHi
-y = countLow
-
-  countSum = x + y
-
+  reset()
+  getSum()
   if (countSum > 0) {
     totalCount.innerHTML = `Seems like your Highs are ${countSum}. KEEP IT UP.`
   } else if (countSum < 0) {
