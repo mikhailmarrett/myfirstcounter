e40f3e9f8178cfff1a1abdbde5c853a254d81d09
Mikhail Marrett <mikhailmarrett@Mikhails-MacBook-Air.local>
2022-06-26 16:49:22 -0400

    update script

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const totalCount = document.getElementById("total")
 let count = 0
 let countSum = 0
 
+totalCount.innerHTML = `How many High moments you had this week?`
+counterEl.style.color = "gray"
+
+
 function textColor() {
   if (count > 0) {
     counterEl.style.color = "green"
@@ -16,31 +20,21 @@ function textColor() {
 function increase() {
   count += 1
   countHi = count
-  counterEl.innerHTML = countHi
+  counterEl.innerHTML = count
   textColor()
 }
 
 function decrease() {
   count -= 1
   countLow = count
-  counterEl.innerHTML = countLow
-  textColor()
-}
-
-function clearOut() {
-  count = 0
-  countHi = 0
-  countLow = 0
-  countSum = 0
   counterEl.innerHTML = count
-  totalCount.innerHTML = ""
   textColor()
 }
 
 function reset() {
   count = 0
   counterEl.innerHTML = count
-  totalCount.innerHTML = ""
+  totalCount.innerHTML = `How many High moments did you have this week?`
   textColor()
 }
 
@@ -50,10 +44,23 @@ function getSum() {
 
 function printTotal() {
   reset()
+  totalCount.innerHTML = `How many Low moments did you have this week?`
   getSum()
   if (countSum > 0) {
-    totalCount.innerHTML = `Seems like your Highs are ${countSum}. KEEP IT UP.`
+    totalCount.innerHTML = `Seems like your Highs for this week are greater than Lows by ${countSum}. KEEP IT UP.`
   } else if (countSum < 0) {
-    totalCount.innerHTML = `Seems like your Lows are ${countSum}. BETTER LUCK NEXT TIME.`
+    totalCount.innerHTML = `Seems like your Lows for this week are greater than Highs by ${Math.abs(countSum)}. BETTER LUCK NEXT TIME.`
+  } else {
+    totalCount.innerHTML = `YOU BROKE EVEN THIS WEEK.`
   }
+}
+
+function clearOut() {
+  count = 0
+  countHi = 0
+  countLow = 0
+  countSum = 0
+  counterEl.innerHTML = count
+  totalCount.innerHTML = `How many High moments did you have this week?`
+  textColor()
 }
